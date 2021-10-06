31b85bc1dbdc84868f539f8ad3eab58a7a8c7164
akseltorgerson <akseltorgerson@gmail.com>
2021-10-05 22:45:53 -0500

    reworked tpuv1

--- hw/tpuv1.sv
+++ hw/tpuv1.sv
@@ -1,169 +1,164 @@
 module tpuv1
-  #(
+	#(
     parameter BITS_AB=8,
     parameter BITS_C=16,
     parameter DIM=8,
     parameter ADDRW=16,
     parameter DATAW=64
-    )
-   (
-    input clk, rst_n, r_w, // r_w=0 read, =1 write
+  )
+  (
+  	input clk, rst_n, r_w, // r_w=0 read, =1 write
     input [DATAW-1:0] dataIn,
     output [DATAW-1:0] dataOut,
     input [ADDRW-1:0] addr
-   );
+  );
    
+	/* -------------------- Localparams ------------------ */
 	localparam ROWBITS = $clog2(DIM);
 	localparam COUNTER_BITS = $clog2(DIM*4);
 	
-	///////////////////////////////
-	/// intermediate signals
-	//////////////////////////////
-	logic WrEn_SA, WrEn_A;
-	logic signed [BITS_AB-1:0] A [DIM-1:0];
-	logic signed [BITS_AB-1:0] B [DIM-1:0];
-	logic signed [BITS_C-1:0] Cin [DIM-1:0];
-	logic signed [BITS_C-1:0] Cout [DIM-1:0];
-	logic signed [BITS_AB-1:0] Amem_int [DIM-1:0];
-	logic signed [BITS_AB-1:0] Bmem_int [DIM-1:0];
-	logic signed [BITS_C-1:0] out_reg [DIM-1:0];
-	
-	genvar rowcolA, rowcolB, rowcolC, ballsOut, rowcolC2;
-	
-	// control signals
-	logic startCount;
-	logic loadB;
-	logic en;
-	logic unsigned [COUNTER_BITS-1:0] counter;
-	
-	////////////////////////////////
-	///// DUTS
-	////////////////////////////////
-	
-	systolic_array #(
-			.BITS_AB(BITS_AB),
-                    	.BITS_C(BITS_C),
-                    	.DIM(DIM))
-                    	systolic_array_DUT (.clk(clk),
-                        		.rst_n(rst_n),
-                                        .en(en),
-                                        .WrEn(WrEn_SA),
-                                        .A(Amem_int),
-                                        .B(Bmem_int),
-                                        .Cin(Cin),
-                                        .Crow(addr[6:4]),
-                                        .Cout(Cout)
-                                        );
-	
-	memA #(
-		.BITS_AB(BITS_AB),
-          	.DIM(DIM))
-         	 memA_DUT (.clk(clk),
-                    .rst_n(rst_n),
-                    .en(en),
-                    .WrEn(WrEn_A),
-                    .Ain(A),
-                    .Arow(addr[5:3]),
-                    .Aout(Amem_int)
-                    );
-	
-	memB #(
-		.BITS_AB(BITS_AB),
-          	.DIM(DIM))
-          	memB_DUT (.clk(clk),
-                    .rst_n(rst_n),
-                    .en(en | loadB),
-                    .Bin(B),
-                    .Bout(Bmem_int)
-                    );
-	
-	/////////////////////////
-	// enable
-	/////////////////////////
-	assign en = |counter;
+	/* -------------------- Interconnects ---------------- */
+	logic signed [BITS_AB-1:0] memA_in [DIM-1:0];
+	logic signed [BITS_AB-1:0] memA_out [DIM-1:0];
+	logic signed [BITS_AB-1:0] memB_in [DIM-1:0];
+	logic signed [BITS_AB-1:0] memB_out [DIM-1:0];
+	logic signed [BITS_C-1:0] macC_in [DIM-1:0];
+	logic signed [BITS_C-1:0] macC_out [DIM-1:0];
+	logic [DATAW-1:0] cWord;
+	logic [ROWBITS-1:0] aRow, cRow;
 
-	/////////////////////////
-	// counter 
-	/////////////////////////
+	/* -------------------- Control Signals -------------- */
+	logic WrEn_A, WrEn_B, WrEn_SA, en;
+
+	/* -------------------- Counter Signals -------------- */
+	reg [COUNTER_BITS-1:0] counter;
+	logic startCount;
+
+	/* -------------------- Gen Vars --------------------- */
+	genvar i;
+	
+	/* -------------------- Modules ---------------------- */
+	systolic_array #(.BITS_AB(BITS_AB), .BITS_C(BITS_C), .DIM(DIM)) systolic_array_DUT (
+		.clk(clk),
+    .rst_n(rst_n),
+    .en(en),
+    .WrEn(WrEn_SA),
+    .A(memA_out),
+    .B(memB_out),
+    .Cin(macC_in),
+    .Crow(cRow),
+    .Cout(macC_out)
+  );
+
+	memA #(.BITS_AB(BITS_AB), .DIM(DIM)) memA_DUT (
+		.clk(clk),
+    .rst_n(rst_n),
+		.en(en),
+		.WrEn(WrEn_A),
+		.Ain(memA_in),
+		.Arow(aRow),
+		.Aout(memA_out)
+	);
+	
+	memB #(.BITS_AB(BITS_AB), .DIM(DIM)) memB_DUT (
+		.clk(clk),
+		.rst_n(rst_n),
+		.en(en | WrEn_B),
+		.Bin(memB_in),
+		.Bout(memB_out)
+	);
+	
+	/* -------------------- Assignments ------------------- */
+	assign en = |counter;
+	assign dataOut = cWord;
+	assign aRow = addr[5:3];
+	assign cRow = addr[6:4];
+
+	/* -------------------- Counter ----------------------- */
 	always_ff @(posedge clk, negedge rst_n) begin
-		if (!rst_n)
+		if (~rst_n)
 			counter <= 0;
-		else if (startCount | en)
+		else if (startCount || en)
 			counter <= counter + 1;
 	end
 
-	/////////////////
-	// generates
-	////////////////
+	/* --------------- Generates Interconnects ------------ */
 	
 	// assign for A
-	//Nate has no conidtional here
 	generate
-		for(rowcolA=0;rowcolA<DIM;++rowcolA) begin
-			assign A[rowcolA] = /*WrEn_A ?*/ dataIn[(((rowcolA+1)*BITS_AB)-1):(rowcolA*BITS_AB)]; //: '0;
+		for(i = 0; i < DIM; i++) begin
+			assign memA_in[i] = WrEn_A ? dataIn[(((i + 1) * BITS_AB) - 1):(i * BITS_AB)] : '0;
 		end
 	endgenerate
 	
 	// assign for B
-	//Nate has a conditional based off of en not off of loadB
-	//If en then load zeros otherwise do the math stuff (our math is right) 
 	generate
-		for(rowcolB=0;rowcolB<DIM;++rowcolB) begin
-			assign B[rowcolB] = en ? '0 : dataIn[((rowcolB+1)*BITS_AB)-1:(rowcolB*BITS_AB)] ;
+		for(i = 0; i < DIM; i++) begin
+			assign memB_in[i] = WrEn_B ? dataIn[((i + 1) * BITS_AB) - 1:(i * BITS_AB)] : '0;
 		end
 	endgenerate
 	
-	// Assign for Cin ( first half )
+	// Assign for Cin (low word)
 	generate
-		for(rowcolC=0;rowcolC<DIM/2;++rowcolC) begin
-			assign Cin[rowcolC] = addr[3] ? Cout[rowcolC] : dataIn[((rowcolC+1)*BITS_C)-1:(rowcolC)*(BITS_C)];
+		for(i = 0; i < DIM/2; i++) begin
+			assign macC_in[i] = addr[3] ? macC_out[i] : dataIn[(((i + 1) * BITS_C) - 1):(i * BITS_C)];
 		end
 	endgenerate
 	
-	// assign for Cin (second half)
+	// assign for Cin (high word)
 	generate
-		for(rowcolC2 = DIM/2; rowcolC2< DIM; ++rowcolC2) begin
-			assign Cin[rowcolC2] = addr[3] ? dataIn[((rowcolC2+1-(DIM/2))*BITS_C)-1:(rowcolC2-DIM/2)*(BITS_C)] : Cout[rowcolC2];
+		for(i = DIM/2; i < DIM; i++) begin
+			assign macC_in[i] = addr[3] ? dataIn[((i + 1 - (DIM/2)) * BITS_C) - 1:(i - DIM/2) * (BITS_C)] : macC_out[i];
 		end
 	endgenerate
 	
 	// assign dataOut
-	generate
-		for(ballsOut=0; ballsOut < DIM/2; ballsOut++) begin
-			assign dataOut[((BITS_C)*ballsOut) + (BITS_C - 1):(BITS_C*ballsOut)] = addr[3] ? out_reg[ballsOut + DIM/2] : out_reg[ballsOut];
-		end
-	endgenerate
+	//generate
+	//	for(ballsOut=0; ballsOut < DIM/2; ballsOut++) begin
+	//		assign dataOut[((BITS_C)*ballsOut) + (BITS_C - 1):(BITS_C*ballsOut)] = addr[3] ? out_reg[ballsOut + DIM/2] : out_reg[ballsOut];
+	//	end
+	//endgenerate
 	
-	///////////////////////
-	// case statement
-	///////////////////////
+	/* -------------------- Combinational --------------------- */
 	always_comb begin
-		// set the input values to be 0
-		for(int rowcol=0;rowcol<DIM;++rowcol) begin
-		  out_reg[rowcol] = {BITS_C{1'b0}};
-		end
-		
+		// 
+		// Defaults
+		//		
 		WrEn_A = 0;
+		WrEn_B = 0;
 		WrEn_SA = 0;
-		loadB = 0;
 		startCount = 0;
+		cWord = 0;
 
 		case (addr[11:8])
 			// write to A
 			4'h1: begin
-				WrEn_A = 1;
+				if (r_w) begin
+					WrEn_A = 1;
+				end
 			end
 			// write to B
 			4'h2: begin
-				loadB = 1;
-      			end
+				if (r_w) begin
+					WrEn_B = 1;
+				end
+			end
 			// read / write to C
 			4'h3: begin
-				if (!r_w) begin
-					out_reg = Cout;
+				if (r_w) begin
+					WrEn_SA = 1;
 				end
 				else begin
-					WrEn_SA = 1;
+					if (addr[3]) begin
+						for (int i = DIM/2; i < DIM; i++) begin
+							cWord |= (macC_out[i] << ((i - (DIM/2)) * BITS_C));
+						end
+					end
+					else begin
+						for (int i = 0; i < DIM/2; i++) begin
+							cWord |= (macC_out[i] << (i * BITS_C));
+						end
+					end
 				end
 			end
 			// matmul
